18b689f9d073b654747c027649e3bb7f659a81f7
Leandro <leandrochorolque@gmail.com>
2016-11-22 16:05:38 -0300

    se rompio tooodooo

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,34 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="inicio")
      */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
+    public function indexAction(Request $request) {
+        $repository = $this->getDoctrine()
+                ->getRepository('AppBundle:Producto');
+
+        $query = $repository->createQueryBuilder('p')
+                ->where('p.stock < p.stockMinimo')
+                ->orderBy('p.nombre', 'ASC')
+                ->getQuery();
+        $cantidad = $query->getResult()->count();
+        $repository2 = $this->getDoctrine()
+                ->getRepository('AppBundle:Pedido');
+
+        $query2 = $repository2->createQueryBuilder('p')
+                ->where('p.fechaCierre = null')
+                ->orderBy('p.id', 'ASC')
+                ->getQuery();
+
+        $productos = $query->getResult();
+        $stockCritico = array('productos' => $productos, 'cantidad' => $cantidad,);
+        $pedidosAbiertos = $query2;
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+                    'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'), 'stockCritico' => $stockCritico, 'pedidosAbiertos' => $pedidosAbiertos,
         ]);
     }
+
 }
