5d5ff8d286bc184ef4b35cc3da819de99800f39f
Milton Sosa <milton.sosa.22@gmail.com>
2016-12-06 00:02:38 -0300

    terminando detalles

--- web/js/NewSend.js
+++ web/js/NewSend.js
@@ -1,8 +1,14 @@
 $(function() {
-  $(".chosen-select").chosen({no_results_text: "Oops, no hay resultados!"});
+  $('#productSelect').select2();
+
   $('#productSelect').change(function(){
     if ($('#product_'+$(this).val()).length == 0) {
       $('#listProduct').append('<tr id="product_'+$(this).val()+'"><td>'+$(this).val()+'</td><td>'+$('#productSelect option:selected').text()+'</td><td><input type="number" class="form-control productCant" view=false name="producto['+$(this).val()+']" max="'+$('#productSelect option:selected').attr('stock')+'"value="0" stockMinimo="'+$('#productSelect option:selected').attr('stockmin')+'"/></td><td>'+$('#productSelect option:selected').attr('stock')+'</td><td class="table-operations"><span role="button" onclick="remove('+$(this).val()+',\''+$('#productSelect option:selected').text()+'\')" title="eliminar"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></span></td></tr>');
+      $('#productSelect option:selected').remove();
+      if ($('#productosAdd option').length == 1){
+        $('#productosAdd option').remove();
+        $('#productosAdd').append('<option>No hay productos por agregar</option>');
+      }
       $('.productCant').keyup(function(event){
         if(event.keyCode != 13) {
           var result = $(this).attr('max')-$(this).val();
@@ -44,27 +50,4 @@ $(function() {
         }
     }
   });
-      // $('#appbundle_pedido_proveedor').change(function() {
-      //   $.ajax({
-      //     method: "POST",
-      //     url: url,
-      //     data: {id: $(this).val()}
-      //   })
-      //     .done(function( data ) {
-      //       $('#productosAdd > option').remove();
-      //       if (data.bien.length == 0) {
-      //         $('#productosAdd').append('<option>No hay productos por agregar</option>');
-      //       }else{
-      //         $('#productosAdd').append('<option>Seleccione un producto</option>');
-      //       }
-      //       $.each(data.bien, function (index, product) {
-      //         $('#productosAdd').append('<option value="'+product.id+'">'+product.nombre+'</option>');
-      //       });
-      //       $('#listProduct > tr').remove();
-      //       $.each(data.criticos, function (index, product) {
-      //         $('#listProduct').append('<tr id="product_'+product.id+'"><td>'+product.id+'</td><td>'+product.nombre+'</td><td><input type="number" class="form-control" name="producto['+product.id+']" value="'+(product.stockIdeal-product.stock)+'"/></td><td class="table-operations"><span role="button" onclick="remove('+product.id+',\''+product.nombre+'\')"  title="eliminar"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></span></td></tr>');
-      //       });
-      //     });
-      //  });
-      //
 });
